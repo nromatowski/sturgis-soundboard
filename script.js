3500d8ee86c6bc9bf1490b1eb39e3b67c09903e6
nromatowski <nicholas.romatowski@gmail.com>
2021-02-22 21:25:47 -0500

    anananan
    ananananan

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log("hello");
+
+const btn1 = document.querySelector(".button1");
+const btn2 = document.querySelector(".button2");
+const btn3 = document.querySelector(".button3");
+const btn4 = document.querySelector(".button4");
+const btn5 = document.querySelector(".button5");
+const btn6 = document.querySelector(".button6");
+const btn7 = document.querySelector(".button7");
+const btn8 = document.querySelector(".button8");
+const btn9 = document.querySelector(".button9");
+const btn10 = document.querySelector(".button10");
+const btn11 = document.querySelector(".button11");
+const btn12 = document.querySelector(".button12");
+
+btn1.addEventListener("click", function () {
+  console.log("you clicked 1");
+});
